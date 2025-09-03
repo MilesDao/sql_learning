646eda95f51a9d72098c1122316182cccfa75dc7
Daotrung1311 <trungdao131105@gmail.com>
2025-09-03 11:55:12 +0700

    init

---
+++ sql_chapter1.sql
@@ -0,0 +1,5 @@
+SELECT * FROM movies WHERE industry = "Bollywood";
+select distinct industry from movies;
+select * from movies where title like "%THOR%";
+select * from movies where title like "%AMERICA%";
+select * from moviesdb.movies
